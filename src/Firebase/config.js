f750d2acee7943c45562b1e2cfa0c12d51487912
WilliamBostrom <133894488+WilliamBostrom@users.noreply.github.com>
2024-03-20 14:24:16 +0100

    hämtar collection  från firebase

---
+++ src/Firebase/config.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAn_EhQuKM6figUrA4H0XHvlCOHHEaots0",
+  authDomain: "mymoney-ff0f8.firebaseapp.com",
+  projectId: "mymoney-ff0f8",
+  storageBucket: "mymoney-ff0f8.appspot.com",
+  messagingSenderId: "664125429911",
+  appId: "1:664125429911:web:493b2a36999434239fb1d2",
+};
+
+//init firestore
+initializeApp(firebaseConfig);
+
+// init firestore
+const db = getFirestore();
+
+export { db };
